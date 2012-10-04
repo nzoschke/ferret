d9d7c1cef86caa6b01f2f98cdfa7b40559d9c64b
Noah Zoschke <noah@heroku.com>
2012-10-03 17:19:01 -0700

    better setup assertions

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,10 +2,12 @@
 
 [ -n "$APP" ]            || { echo "error: APP required"; exit 1; }
 [ -n "$HEROKU_API_KEY" ] || { echo "error: HEROKU_API_KEY required"; exit 1; }
-
-[ -n "$SSH_PRIVATE_KEY" ] && [ -n "$SSH_PUBLIC_KEYS" ] && { echo "warning: SSH vars already set"; exit 0; }
+[ -f $HOME/.ssh/id_rsa ] && { echo "error: ssh private key already exists"; exit 1; }
 
 mkdir -p $HOME/.ssh
 ssh-keygen -f $HOME/.ssh/id_rsa -N "" -t rsa
 heroku keys:add   --app $APP
 heroku config:set --app $APP SSH_PRIVATE_KEY="$(< $HOME/.ssh/id_rsa)" SSH_PUBLIC_KEY="$(< $HOME/.ssh/id_rsa.pub)"
+
+# verify setup
+git clone git@heroku.com:$APP.git && echo "Setup complete" || echo "Setup FAILED"
