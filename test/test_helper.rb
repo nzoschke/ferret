12fafb34b30d56cf662e2524f8b6e7a5052b81f0
Noah Zoschke <noah@heroku.com>
2012-10-23 14:48:11 -0700

    clean up minitests

---
+++ test/test_helper.rb
@@ -0,0 +1,22 @@
+require "minitest/autorun"
+require "stringio"
+require "tmpdir"
+
+ENV["XID"]  = "deadbeef"
+
+$logdevs = [StringIO.new]
+
+require_relative "../lib/ferret"
+
+class TestBase < MiniTest::Unit::TestCase
+  def setup
+    ENV["TEMP_DIR"] = Dir.mktmpdir
+    $logdevs[0].rewind
+    $logdevs[0].truncate(0)
+  end
+
+  def logs
+    $logdevs[0].rewind
+    $logdevs[0].read.gsub(/val=[0-9.]+/, "val=X")
+  end
+end
