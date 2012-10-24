337d7589fb0e8fb460e9f2d0b02f11a55ad0c42a
Noah Zoschke <noah@heroku.com>
2012-10-24 12:43:09 -0700

    build with ferret buildpack

--- app/ssh_tcp_receive/.profile.d/ferret.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export HOST=$(/sbin/ifconfig | sed -n 's/.*inet addr:\([0-9.]\+\)\s.*/\1/p' | head -1)
-export GEM_HOME=/tmp/gems
-export PATH=$GEM_HOME/bin/:${PATH}
-export USER=$(whoami)
