2ad340d074e85dc60af6e6d25bf7ccb05781e659
Noah Zoschke <noah@heroku.com>
2012-12-03 14:21:35 -0800

    simplify readme for setup script

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,24 +1,31 @@
-export 																	 \
-    FERRET_USER=fode													 \
-	UNPRIVILEGED_HEROKU_API_KEY=6413536ddc3a5a10e8f573ff28273b2521f1ea85 \
-	UNPRIVILEGED_GMAIL_USER=heroku.ferret.dev@gmail.com
+#!/bin/bash
 
-export APP=ferret-dev-${FERRET_USER}
-heroku apps:create $APP
-heroku config:set  --app ${APP}                                           \
-	APP=$APP                                                              \
-    GMAIL_USER=$UNPRIVILEGED_GMAIL_USER                                   \
-    HEROKU_API_KEY=$UNPRIVILEGED_HEROKU_API_KEY
+[ "$UNPRIVILEGED_HEROKU_API_KEY" ] || { echo UNPRIVILEGED_HEROKU_API_KEY required; exit 1; }
 
-export ORG=ferret-dev 
+# client and account pre-reqs
 heroku plugins:install https://github.com/heroku/manager-cli.git
 heroku plugins:install https://github.com/ddollar/heroku-anvil.git
-heroku manager:add_user													\
+heroku sudo passes:add logplex-beta-program
+
+export 																	              \
+  FERRET_USER=$(whoami)													      \
+  UNPRIVILEGED_GMAIL_USER=heroku.ferret.dev@gmail.com
+	
+export APP=ferret-${FERRET_USER}
+heroku apps:create $APP
+heroku config:set --app ${APP}                        \
+	APP=$APP                                            \
+  GMAIL_USER=$UNPRIVILEGED_GMAIL_USER                 \
+  HEROKU_API_KEY=$UNPRIVILEGED_HEROKU_API_KEY
+
+export ORG=ferret-dev 
+heroku manager:add_user													      \
 	--org $ORG --user ${UNPRIVILEGED_GMAIL_USER} --role admin --app ${APP}
 heroku manager:transfer --to $ORG --app ${APP}
-heroku sudo passes:add logplex-beta-program
 
 heroku build -b https://github.com/nzoschke/buildpack-ferret.git -r $APP
 heroku run "test/ferret; test/ferret_online" --app ${APP}
+
+heroku drains:add --app ${APP} ${L2MET_URL}
 
 
