1fd1d18322f9fcef8ba72a57987fd0025ef6190f
Eric Fode <Eric@heroku.com>
2012-12-03 14:36:32 -0800

    removed old varibles

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,21 +6,13 @@
 heroku plugins:install https://github.com/heroku/manager-cli.git
 heroku plugins:install https://github.com/ddollar/heroku-anvil.git
 heroku sudo passes:add logplex-beta-program
-
-export 																	              \
-  FERRET_USER=$(whoami)													      \
-  UNPRIVILEGED_GMAIL_USER=heroku.ferret.dev@gmail.com
-	
-export APP=ferret-${FERRET_USER}
 heroku apps:create $APP
 heroku config:set --app ${APP}                        \
-	APP=$APP                                            \
-  GMAIL_USER=$UNPRIVILEGED_GMAIL_USER                 \
+  APP=$APP                                            \
   HEROKU_API_KEY=$UNPRIVILEGED_HEROKU_API_KEY
 
-export ORG=ferret-dev 
 heroku manager:add_user													      \
-	--org $ORG --user ${UNPRIVILEGED_GMAIL_USER} --role admin --app ${APP}
+  --org $ORG --user ${GMAIL_USER} --role admin --app ${APP}
 heroku manager:transfer --to $ORG --app ${APP}
 
 heroku build -b https://github.com/nzoschke/buildpack-ferret.git -r $APP
