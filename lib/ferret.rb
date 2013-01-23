d7007bcb0e31ac615292cd5e61f6873886d6a4d9
Eric Fode <Eric@heroku.com>
2013-01-23 14:22:45 -0800

    added quotes to source

--- lib/ferret.rb
+++ lib/ferret.rb
@@ -3,24 +3,37 @@ require "securerandom"
 require "timeout"
 require "tmpdir"
 require "redis"
+require "statsample"
+
+$redis                      = Redis.connect :url => ENV["OPENREDIS_URL"]
 
 
+$name = ENV["NAME"]               ||= File.basename($0, File.extname($0)) # e.g. git_push
 ENV["APP"]                ||= "ferret"
 ENV["FERRET_DIR"]         ||= File.expand_path(File.join(__FILE__, "..", ".."))
 ENV["ORG"]                ||= "ferret-dev"
 ENV["SCRIPT"]             ||= File.expand_path($0) # $FERRET_DIR/tests/git/push or $FERRET_DIR/tests/unit/test_ferret.rb
 ENV["TEMP_DIR"]           ||= Dir.mktmpdir
-Thread.current[:xid]         = SecureRandom.hex(4)
-ENV["FREQ"]               ||= "30"
-ENV["NAME"]               ||= File.basename($0, File.extname($0)) # e.g. git_push
+ENV["FREQ"].to_i          || 30.to_s
+ENV["INTERVAL"]           ||= 240.to_s #how long to track for in seconds when doing math (time-series window)
+Thread.current[:xid]        = SecureRandom.hex(4)
 ENV["SERVICE_LOG_NAME"]   ||= "#{ENV["APP"]}.#{ENV["NAME"]}" # e.g. ferret-noah.git-push #slave app name
 ENV["SERVICE_APP_NAME"]   ||= ENV["USER"] + "-" + ENV["SERVICE_LOG_NAME"].gsub(/[\._]/, '-') # e.g. ferret-noah-git-push #used for deploying slave app
 
-$redis                      = Redis.connect :url => ENV["OPENREDIS_URL"]
 $log_prefix               ||= { app: "#{ENV["APP"]}"}
 $logdevs                  ||= [$stdout, IO.popen("logger", "w")]
 $threads                    = []
 $lock                       = Mutex.new
+
+if !$redis.hexists($name,"app")
+  $redis.hset($name,"app",ENV["APP"])
+  $redis.hset($name,"freq",ENV["FREQ"].to_i)
+  $redis.hset($name, "status","starting")
+  $redis.hset($name, "interval", ENV["INTERVAL"].to_i)
+
+end
+$freq = $redis.hget($name,"freq").to_i
+$interval = $redis.hget($name, "interval").to_i
 
 trap("EXIT") do
   log fn: :exit
@@ -28,10 +41,50 @@ trap("EXIT") do
   FileUtils.rm_rf ENV["TEMP_DIR"]
 end
 
+
 class Hash
   def rmerge!(h)
     replace(h.merge(self))
   end
+end
+
+def dostats()
+  lasttime  = Thread.current[:lasttime] || 0
+  drop      = (Time.now.to_i - lasttime)/$interval
+  uptimesa   = Thread.current[:uptimes]
+  timesa     = Thread.current[:times]
+  uaveragesa = Thread.current[:uavarages]
+  taveragesa = Thread.current[:tavarages]
+  if drop > 0
+    uptimesa.slice!(0..drop)
+    timesa.slice!(0..drop)
+    uaveragesa.slice!(0..drop)
+    taveragesa.slice!(0..drop)
+  end
+
+
+  uaverages = uaveragesa.to_vector(:scale)
+  taverages = taveragesa.to_vector(:scale)
+  uptimes   = uptimesa.to_vector(:scale)
+  times     = timesa.to_vector(:scale)
+
+  uaveragesa << times.mean
+  taveragesa << uptimes.mean
+
+  $redis.hset($name, "#{Thread.current[:name]}.uptime.mean", uptimes.mean)
+  $redis.hset($name, "#{Thread.current[:name]}.uptime.min", uptimes.min)
+  $redis.hset($name, "#{Thread.current[:name]}.uptime.max", uptimes.max)
+  $redis.hset($name, "#{Thread.current[:name]}.uptime.last", uptimesa.last)
+  $redis.hset($name, "#{Thread.current[:name]}.uptime.average_variance", uaverages.variance)
+  $redis.hset($name, "#{Thread.current[:name]}.uptime.variance", uptimes.variance)
+
+  $redis.hset($name, "#{Thread.current[:name]}.time.mean", times.mean)
+  $redis.hset($name, "#{Thread.current[:name]}.time.min", times.min)
+  $redis.hset($name, "#{Thread.current[:name]}.time.max", times.max)
+  $redis.hset($name, "#{Thread.current[:name]}.time.last", timesa.last)
+  $redis.hset($name, "#{Thread.current[:name]}.time.variance", times.variance)
+  $redis.hset($name, "#{Thread.current[:name]}.time.average_variance", taverages.variance)
+  Thread.current[:lasttime] = Time.now.to_i
 end
 
 def run(opts={})
@@ -97,18 +150,28 @@ def run_every_time(&block)
 end
 
 def bash(opts={})
+
   opts[:bash] = opts[:stdin]
   test(opts)
 end
 
-def test(opts={}, &blk)
+def test(opts={}, &blk) 
+  
   opts.rmerge!(name: "test", retry: 1, pattern: nil, status: 0, timeout: 180)
 
+  Thread.current[:times]     ||= Array.new
+  Thread.current[:uptimes]   ||= Array.new
+  Thread.current[:uavarages] ||= Array.new
+  Thread.current[:tavarages] ||= Array.new 
+  Thread.current[:failcount] ||= 0
+  
+  times = Thread.current[:times]
+  uptimes = Thread.current[:uptimes]
   script = ENV["SCRIPT"].chomp(File.extname(ENV["SCRIPT"]))           # strip extension
   script = script.split("/").last(2).join("/")                        # e.g. git/push or unit/test_ferret
   ENV["TARGET_APP"] = "#{ENV["APP"]}-#{script}".gsub(/[\/_]/, "-")    # e.g. ferret-git-push or ferret-unit-test-ferret
-  source = "#{script}.#{opts[:name]}".gsub(/\//, ".").gsub(/_/, "-")  # e.g. git.push.test
-
+  source = "\"#{script}.#{opts[:name]}\"".gsub(/\//, ".").gsub(/_/, "-")  # e.g. git.push.test
+  Thread.current[:name] = opts[:name]
   begin
     Timeout.timeout(opts[:timeout]) do
       opts[:retry].times do |i|
@@ -144,26 +207,34 @@ def test(opts={}, &blk)
           log source: source, i: i, status: status, measure: "success"
           log source: source, i: i, val: 100, measure: "uptime"
           log source: source, i: i, at: :return, val: "%0.4f" % (Time.now - start), measure: "time"
-          $redis.set(source,"up")
+          uptimes << 100
+          times << (Time.now - start)
+          $redis.hset($name,"#{Thread.current[:name]}.status","up")
+          $redis.hset($name,"status","up")
           return success # break out of retry loop
         else
-          Thread.current[:failcount] = Thread.current[:failcount]+1
-          $redis.set(source,"yellow")
+          $redis.hset($name, "#{Thread.current[:name]}.status" ,"yellow")
+          $redis.hset($name, "status", "yellow" )
           out.each_line { |l| log source: source, i: i, at: :failure, out: "'#{l.strip}'" }
 
-          if Thread.current[:failcount] > 5 
-            $redis.set(source,"down")
           end
           # only measure last failure
           if i == opts[:retry] - 1
             log source: source, i: i, status: status, measure: "failure"
             log source: source, i: i, val: 0, measure: "uptime"
+            uptimes << 0
+            times << (Time.now - start)
+            Thread.current[:failcount] = Thread.current[:failcount]+1
+          if Thread.current[:failcount] > 5
+            $redis.hset($name,"#{Thread.current[:name]}.status","down")
+            $redis.hset($name,"status","down") 
           else
             log source: source, i: i, status: status
           end
           log source: source, i: i, at: :return, val: "%0.4f" % (Time.now - start)
         end
       end
+      dostats
     end
   rescue Timeout::Error
     log source: source, at: :timeout, val: opts[:timeout]
